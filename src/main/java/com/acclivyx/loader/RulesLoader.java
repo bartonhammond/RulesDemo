20782a70569ff7b17fbd3a9b93d5b16cd4773375
Barton Hammond <bartonhammond@gmail.com>
2012-09-09 13:14:52 -0500

    document classes more

--- src/main/java/com/acclivyx/loader/RulesLoader.java
+++ src/main/java/com/acclivyx/loader/RulesLoader.java
@@ -10,9 +10,33 @@ import com.acclivyx.rules.sequenceintegerrules.AverageIsGreaterThenRule;
 import com.acclivyx.rules.sequenceintegerrules.TwoArgsGreaterThenRule;
 import com.acclivyx.rules.sequenceintegerrules.TwoArgsSpanGreaterThenRule;
 
+/**
+ * Loads appropriate Rules for the type of Arguments to be processed.
+ * 
+ * As new types of Arguments are added to the system, there would be associated
+ * new Rules. The Loader needs to be updated to associate the Arguments to the
+ * Rules. Certain Rules are only applicable to specific Arguments.
+ * 
+ * If this were to grow the implementation could be changed to dynamically find
+ * all the classes on the classpath that implement the Rule and a verifcation
+ * could be introduced to determine if the Rule supports the Argument
+ * dynamically.
+ * 
+ * But this exercise, this implementation is sufficient.
+ * 
+ * @author Barton
+ * 
+ */
 
 public class RulesLoader {
-
+	/**
+	 * Determine which instance of the RuleArgs is
+	 * passed in and associated the Rules that 
+	 * support them.
+	 * 
+	 * @param args arguments 
+	 * @return
+	 */
 	public Collection<IRule> getRulesForArgs(RuleArgs args) {
 		Collection<IRule> rules = new ArrayList<IRule>();
 		if (args instanceof RuleIntegerArgs) {
@@ -22,6 +46,5 @@ public class RulesLoader {
 		}
 		return rules;
 	}
-
 
 }
